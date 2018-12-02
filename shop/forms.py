a10b2a53f3eb8b27642ff005fd5eb787d3a8af39
imac <kovoli1985@gmail.com>
2018-12-02 17:39:31 +0100

    123

--- shop/forms.py
+++ shop/forms.py
@@ -6,3 +6,15 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control',
+                                    'placeholder': 'Введите ваше имя'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control',
+                                             'placeholder': 'Введите ваш емайл'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': '5'})
+
+        }
+        labels = {
+            'name': 'Имя',
+            'body': 'Ваш отзыв'
+        }
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Product, Category
+from .models import Product, Category, Comment
+from .forms import CommentForm
 
 
 def home_page(request):
@@ -11,9 +12,25 @@ def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     breadcrumbs = Category.get_ancestors(product.category)
 
+    comments = product.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.product = product
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
 
     return render(request, 'shop/single_product.html', {'product': product,
-                                                        'breadcrumbs': breadcrumbs})
+                                                        'breadcrumbs': breadcrumbs,
+                                                        'comments': comments,
+                                                        'new_comment': new_comment,
+                                                        'comment_form': comment_form})
 
 
 
