c0c4dcd4c2c8fc1bf58f13d8b11056ff39da83f5
imac <kovoli1985@gmail.com>
2019-01-04 17:43:57 +0100

    123

--- discounts/models.py
+++ discounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
+# Удаление фотографии после удаления скидки
+import os
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
-# Create your models here.
+
+class DiscountProduct(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    param = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    oldprice = models.DecimalField(max_digits=10, decimal_places=2)
+    url = models.URLField()
+    discount_image = ProcessedImageField(upload_to='deals_images/%Y/%m', blank=True, max_length=250,
+                                      processors=[ResizeToFit(None, 255)],
+                                      format='JPEG',
+                                      options={'quality': 70})
+
+    class Meta:
+        verbose_name = 'Скидка'
+        verbose_name_plural = 'Скидки'
+
+    def __str__(self):
+        return self.name
+
+
+# Удаляет фотографию после удаления статьи
+def delete_file(path):
+    # Deletes file from filesystem.
+    if os.path.isfile(path):
+        os.remove(path)
+
+
+@receiver(pre_delete, sender=DiscountProduct)
+def delete_img_pre_delete_post(sender, instance, *args, **kwargs):
+    if instance.deals_image:
+        delete_file(instance.deals_image.path)
+
--- ep_proj/settings.py
+++ ep_proj/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'shop.apps.ShopConfig',
+    'discounts.apps.DiscountsConfig',
     # -- extensions --
     'mptt',
     'ckeditor',
