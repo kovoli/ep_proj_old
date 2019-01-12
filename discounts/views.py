40e9ede4851872d303ad173bc45d510ce2879979
imac <kovoli1985@gmail.com>
2019-01-12 13:06:04 +0100

    123

--- discounts/views.py
+++ discounts/views.py
@@ -9,7 +9,8 @@ from shop.views import menu
 def discount_product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    product_list = DiscountProduct.objects.all()
+    list_pro = DiscountProduct.objects.all()
+    product_list = helpers.pg_records(request, list_pro, 6)
     context = {'category': category, 'categories': categories, 'product_list': product_list, 'menu': menu(request)}
 
     if category_slug:
@@ -18,22 +19,25 @@ def discount_product_list(request, category_slug=None):
         categories = category.get_descendants(include_self=True)
         breadcrumbs = Category.get_ancestors(category, include_self=True)
 
-        product_list = product_list.filter(category__in=Category.objects.get(id=category.id)\
+        list_pro = DiscountProduct.objects.filter(category__in=Category.objects.get(id=category.id)\
                                                                 .get_descendants(include_self=True))
 
-        vendors_ids = product_list.values_list('vendor_id', flat=True).order_by().distinct()
+        vendors_ids = list_pro.values_list('vendor_id', flat=True).order_by().distinct()
         vendors = Vendor.objects.filter(id__in=vendors_ids)
         filter_discount.fields['brand'].queryset = Vendor.objects.filter(id__in=vendors_ids)
+
+        product_list = helpers.pg_records(request, list_pro, 12)
         context = {'category': category, 'categories': categories,
                    'product_list': product_list, 'breadcrumbs': breadcrumbs,
                    'vendors': vendors, 'filter_discount': filter_discount,
                    'menu': menu(request)}
+
         if filter_discount.is_valid():
             if filter_discount.cleaned_data['brand']:
                 print(filter_discount.cleaned_data['brand'])
                 list_pro = DiscountProduct.objects.filter(category__in=Category.objects.get(id=category.id)\
-                                                                       .get_descendants(include_self=True))\
-                                                                       .filter(vendor__in=filter_discount.cleaned_data['brand'])
+                                                                                       .get_descendants(include_self=True))\
+                                                                                       .filter(vendor__in=filter_discount.cleaned_data['brand'])
                 product_list = helpers.pg_records(request, list_pro, 100)
                 context = {'category': category, 'categories': categories,
                            'product_list': product_list, 'breadcrumbs': breadcrumbs,
